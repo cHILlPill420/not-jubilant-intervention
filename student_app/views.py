67372f1dd83528ebc16504e44b3d92b7e324711b
cHILLPill420 <ankitraya.ar@outlook.com>
2021-06-11 17:26:38 +0545

    detailview, listview p1

--- student_app/views.py
+++ student_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView, DetailView, TemplateView
+from . import models
 
 # Create your views here.
+class SchoolListView(ListView):
+    model = models.School
+    
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'student_app/school_detail.html'
